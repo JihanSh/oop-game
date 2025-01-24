a368c68231fdd2d9c895da70dc378ad8ce370803
Jihan Shamas <jihan.shamas@gmail.com>
2025-01-24 14:06:05 +0100

    create the obstacle and implement its functionality

--- js/main.js
+++ js/main.js
@@ -21,11 +21,52 @@ class Player {
   moveRight() {
     this.positionX++;
     this.updateUI();
-
   }
 }
 
+class Obstacle {
+  constructor() {
+    this.width = 20;
+    this.positionX = Math.floor(Math.random() * (100 - this.width + 1));
+    this.positionY = 90;
+    this.height = 5;
+
+    this.createDomElement();
+  }
+  createDomElement() {
+    //step 1 : cretae the element:
+    this.obstacleElm = document.createElement("div");
+
+    //step 2 : add content or modify (ex: innerHTML...)
+    this.obstacleElm.className = "obstacle";
+    this.obstacleElm.style.width = this.width + "vw";
+    this.obstacleElm.style.height = this.height + "vh";
+    this.obstacleElm.style.left = this.positionX + "vw";
+    this.obstacleElm.style.bottom = this.positionY + "vh";
+    //step 3 : append to the dom (allows us to see it in the html)
+    const parentElm = document.getElementById("board");
+    parentElm.appendChild(this.obstacleElm);
+  }
+  moveDown() {
+    this.positionY--;
+    this.obstacleElm.style.bottom = this.positionY + "vh";
+  }
+}
 const player = new Player();
+const obstacleArr = []; // will store instances of the class obstacle (everything related to the class Obstacle including metthods and specs)
+
+//create obstacles
+setInterval(() => {
+  const newObstacle = new Obstacle();
+  obstacleArr.push(newObstacle);
+}, 2000);
+
+// move obstacle
+setInterval(() => {
+  obstacleArr.forEach((obstacleInstance, i, arr) => {
+    obstacleInstance.moveDown();
+  });
+}, 50);
 
 document.addEventListener("keydown", (event) => {
   if (event.code === "ArrowLeft") {
